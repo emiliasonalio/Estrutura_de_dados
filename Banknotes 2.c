9436ea3cad760da2ff67a674d8b1f49555d6b876
emiliasonalio <134234352+emiliasonalio@users.noreply.github.com>
2023-08-01 16:45:30 -0300

    corrigido exercício

--- Banknotes 2.c
+++ Banknotes 2.c
@@ -2,18 +2,25 @@
 
 int main() {
     int valor;
-    int valorNota[100, 50, 20, 10, 5, 2, 1]
-    int quantNota = 0
+    int valorNota[] = {100, 50, 20, 10, 5, 2, 1};
+    int size = 6;
+    int quantNota = 0;
+    int i;
 
     scanf("%d", &valor);
+    printf("%d\n", valor);
 
-    for(i=0, i<size, i++);
-        if(){
-            valor - valorNota[i];
-            quantNota++
-        }
+    for(i = 0; i < size; i++); //looping que percorre o vetor
+        do {
+            if(valor <= valorNota && valor > 0){
+
+                valor = valor - valorNota[i];
+                quantNota++;
+            }
+        } while (valor<valorNota); //looping que repete a lógica para cada variável (nota)
 
 
+        printf("%d notas(s) de R$ %d,00", quantNota, valorNota[i]);
 
     return 0;
 }
