87548caa2a3cc4843dd728ef1d60986ad1272391
emiliasonalio <134234352+emiliasonalio@users.noreply.github.com>
2023-08-01 22:18:53 -0300

    Exercício 11

---
+++ Interval 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int N, X[N], number, number2;
+    scanf("%d", &N);
+    scanf("%d", &X[N]);
+
+        if (N>10 && N<20){
+            number++;
+        }
+        else{
+            number2++;
+        }
+
+    printf("%d in\n%d out\n", number, number2);
+
+    return 0;
+}
