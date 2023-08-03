2a1f5df68ddafb712365633f04849c6629ca2383
NerusNexuss <126576164+MabyllyMarczal@users.noreply.github.com>
2023-08-03 20:18:26 -0300

    Atualização certa do exercício Interval 2

--- Interval 2.c
+++ Interval 2.c
@@ -1,16 +1,18 @@
-#include <stdio.h>
+##include <stdio.h>
 
 int main() {
-    int N, X[N], number, number2;
+    int N, X, i, number=0, number2=0;
     scanf("%d", &N);
-    scanf("%d", &X[N]);
 
-        if (N>10 && N<20){
-            number++;
-        }
-        else{
-            number2++;
-        }
+    for(i=0; i<N; i++){
+        scanf("%d", &X);
+            if(X>=10 && X<=20){
+                number++;
+            }
+            else{
+                number2++;
+            }
+    }
 
     printf("%d in\n%d out\n", number, number2);
 
