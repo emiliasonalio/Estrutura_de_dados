2bef7c1def76840ca3c29c147bc3f4efb63af6e9
emiliasonalio <emiliasonalio@gmail.com>
2023-08-23 22:11:28 -0300

    Aula 23/08

---
+++ Exercícios Beecrowd/Several Scores with Validation.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    double nota1, nota2, media;
+
+        printf("Escreva as duas notas do aluno:");
+        scanf("%lf""%lf", nota1, nota2);
+            
+        if(nota1 > 10  && nota1 < 1){
+            printf("nota invalida\n");
+        }
+        else if(nota2 > 10  && nota2 < 1){
+            printf("nota invalida\n");
+        }
+        else if(
+            media = (nota1 + nota2) /2
+            printf("media = %lf\n", media)
+        )
+}
